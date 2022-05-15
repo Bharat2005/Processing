64e8b37513337ceabb512e6160d7e4a022202a01
Bharat2005 <69309593+Bharat2005@users.noreply.github.com>
2022-05-15 01:07:16 -0400

    Create BouncingBallAssignment.java
    First project/Assignment with processing. It actually worked... All it does is move the ball in a random direction. if the mouse touches the ball it will change the ball color to red..

---
+++ BouncingBallAssignment.java
@@ -0,0 +1,53 @@
+// Bouncing Ball Assingment 
+float x;
+float y;
+// coordinates of the ball 
+
+float moveX;
+float moveY;
+// how much we must change the coordinates by
+
+void setup(){
+size(600,600);
+
+x = width/2; //assigning a value
+y = 400;
+
+moveX = 5;
+moveY = 5;
+}
+
+void draw()
+{
+background(0);
+
+if(x > width - 25 ){ //check for somethign
+moveX *= -1;//multiplies
+}
+else if(x < 25)
+{
+moveX *= -1;
+}
+
+if (y > height - 25)
+{
+moveY *= -1;
+}
+else if (y < 25)
+{
+  moveY *= -1;
+}
+
+x += moveX;
+y += moveY;
+
+if(dist(x,y, mouseX, mouseY) < 50 ){
+fill(255,0,0);
+println("distance is: " + dist(x,y, mouseX, mouseY) );
+}
+else{//else will run if the statements above are not true
+fill(255);
+}
+
+ellipse(x,y, 50,50);
+}
